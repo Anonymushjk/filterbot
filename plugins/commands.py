2ed20428757a08e5192bc28b58ddd87fd7c1e174
Anonymushjk <94300623+Anonymushjk@users.noreply.github.com>
2022-01-01 10:58:36 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -243,12 +243,11 @@ async def help(client, message):
         reply_markup=InlineKeyboardMarkup(
             [
                 [
-                    InlineKeyboardButton("How to Deploy?", url="https://youtu.be/hkmc3e7U7R4"),
                     InlineKeyboardButton("About Me", callback_data="about_data")
                 ],
                 [
-                    InlineKeyboardButton("BOT Channel", url="https://t.me/TroJanzHEX"),
-                    InlineKeyboardButton("Support Group", url="https://t.me/TroJanzSupport")
+                    InlineKeyboardButton("BOT Channel", url="https://t.me/s3movieshd3"),
+                    InlineKeyboardButton("Support Group", url="https://t.me/s3movieshd3request")
                 ]
             ]
         ),
